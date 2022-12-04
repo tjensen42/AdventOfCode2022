4849de4f403e2b0626f7bc163ced4ba363172497
Tim Jensen <56789534+tjensen42@users.noreply.github.com>
2022-12-04 15:11:43 +0100

    day_03

--- src/bin/day_03.rs
+++ src/bin/day_03.rs
@@ -1,6 +1,13 @@
 use std::fs;
-
 use itertools::Itertools;
+
+fn item_to_priority(c: char) -> usize {
+    if c.is_lowercase() {
+        c as usize - 'a' as usize + 1
+    } else {
+        c as usize - 'A' as usize + 27
+    }
+}
 
 fn main() {
     let input = fs::read_to_string("./input/day_03/input.txt").unwrap();
@@ -11,16 +18,11 @@ fn main() {
         let comp_1 = &line[..line.len() / 2];
         let comp_2 = &line[line.len() / 2..];
 
-        'outer: for c1 in comp_1.as_bytes() {
-            for c2 in comp_2.as_bytes() {
+        'cmp: for c1 in comp_1.chars() {
+            for c2 in comp_2.chars() {
                 if c1 == c2 {
-                    // println!("c1: {}", *c1 as char);
-                    if c1.is_ascii_uppercase() {
-                        sum += *c1 as usize - 'A' as usize + 27;
-                    } else {
-                        sum += *c1 as usize - 'a' as usize + 1;
-                    }
-                    break 'outer;
+                    sum += item_to_priority(c1);
+                    break 'cmp;
                 }
             }
         }
@@ -29,18 +31,13 @@ fn main() {
 
     // Part 2
     sum = 0;
-    for (g1, g2, g3) in input.split("\n").into_iter().tuples() {
-        'outer: for c1 in g1.as_bytes() {
-            for c2 in g2.as_bytes() {
-                for c3 in g3.as_bytes() {
+    for (g1, g2, g3) in input.lines().tuples() {
+        'cmp: for c1 in g1.chars() {
+            for c2 in g2.chars() {
+                for c3 in g3.chars() {
                     if c1 == c2 && c1 == c3 {
-                        // println!("c1: {}", *c1 as char);
-                        if c1.is_ascii_uppercase() {
-                            sum += *c1 as usize - 'A' as usize + 27;
-                        } else {
-                            sum += *c1 as usize - 'a' as usize + 1;
-                        }
-                        break 'outer;
+                        sum += item_to_priority(c1);
+                        break 'cmp;
                     }
                 }
             }
