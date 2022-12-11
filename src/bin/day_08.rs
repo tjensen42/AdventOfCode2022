53f22e0431f0a3e5d06c7493e9ab58aa8d0da076
Tim Jensen <56789534+tjensen42@users.noreply.github.com>
2022-12-11 18:30:30 +0100

    day_08

--- src/bin/day_08.rs
+++ src/bin/day_08.rs
@@ -30,68 +30,32 @@ fn main() {
 // Part 1
 //
 
-fn is_visible(grid: &Vec<&[u8]>, row: usize, column: usize) -> bool {
+fn is_visible(grid: &[&[u8]], row: usize, column: usize) -> bool {
     if row == 0 || row == grid.len() - 1 || column == 0 || column == grid.len() - 1 {
         return true;
     }
-    if is_visible_top(grid, row, column)
-        || is_visible_bottom(grid, row, column)
-        || is_visible_left(grid, row, column)
-        || is_visible_right(grid, row, column)
+    if (0..row).all(|i| grid[i][column] < grid[row][column])
+        || (row + 1..grid.len()).all(|i| grid[i][column] < grid[row][column])
+        || (0..column).all(|i| grid[row][i] < grid[row][column])
+        || (column + 1..grid[row].len()).all(|i| grid[row][i] < grid[row][column])
     {
         return true;
     }
     false
 }
 
-fn is_visible_top(grid: &Vec<&[u8]>, row: usize, column: usize) -> bool {
-    for i in 0..row {
-        if grid[i][column] >= grid[row][column] {
-            return false;
-        }
-    }
-    true
-}
-
-fn is_visible_bottom(grid: &Vec<&[u8]>, row: usize, column: usize) -> bool {
-    for i in row + 1..grid.len() {
-        if grid[i][column] >= grid[row][column] {
-            return false;
-        }
-    }
-    true
-}
-
-fn is_visible_left(grid: &Vec<&[u8]>, row: usize, column: usize) -> bool {
-    for i in 0..column {
-        if grid[row][i] >= grid[row][column] {
-            return false;
-        }
-    }
-    true
-}
-
-fn is_visible_right(grid: &Vec<&[u8]>, row: usize, column: usize) -> bool {
-    for i in column + 1..grid[row].len() {
-        if grid[row][i] >= grid[row][column] {
-            return false;
-        }
-    }
-    true
-}
-
 //
 // Part 2
 //
 
-fn scenic_score(grid: &Vec<&[u8]>, row: usize, column: usize) -> usize {
+fn scenic_score(grid: &[&[u8]], row: usize, column: usize) -> usize {
     scenic_score_up(grid, row, column)
         * scenic_score_down(grid, row, column)
         * scenic_score_left(grid, row, column)
         * scenic_score_right(grid, row, column)
 }
 
-fn scenic_score_up(grid: &Vec<&[u8]>, row: usize, column: usize) -> usize {
+fn scenic_score_up(grid: &[&[u8]], row: usize, column: usize) -> usize {
     let mut score = 0;
     for i in (0..row).rev() {
         score += 1;
@@ -102,7 +66,7 @@ fn scenic_score_up(grid: &Vec<&[u8]>, row: usize, column: usize) -> usize {
     score
 }
 
-fn scenic_score_down(grid: &Vec<&[u8]>, row: usize, column: usize) -> usize {
+fn scenic_score_down(grid: &[&[u8]], row: usize, column: usize) -> usize {
     let mut score = 0;
     for i in row + 1..grid.len() {
         score += 1;
@@ -113,7 +77,7 @@ fn scenic_score_down(grid: &Vec<&[u8]>, row: usize, column: usize) -> usize {
     score
 }
 
-fn scenic_score_left(grid: &Vec<&[u8]>, row: usize, column: usize) -> usize {
+fn scenic_score_left(grid: &[&[u8]], row: usize, column: usize) -> usize {
     let mut score = 0;
     for i in (0..column).rev() {
         score += 1;
@@ -124,7 +88,7 @@ fn scenic_score_left(grid: &Vec<&[u8]>, row: usize, column: usize) -> usize {
     score
 }
 
-fn scenic_score_right(grid: &Vec<&[u8]>, row: usize, column: usize) -> usize {
+fn scenic_score_right(grid: &[&[u8]], row: usize, column: usize) -> usize {
     let mut score = 0;
     for i in column + 1..grid[row].len() {
         score += 1;
